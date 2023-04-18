be5ac3c4f14ed71d698cd5832cccef2f91cb2e99
SairajBhoi <114217464+SairajBhoi@users.noreply.github.com>
2023-04-18 10:31:09 +0530

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -1,7 +1,4 @@
-package com.coveros.demo.helloworld;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 class HelloWorld {
     public static void main(String[] args) {
